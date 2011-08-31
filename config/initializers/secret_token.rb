7b7f60b4a2289812d6c24c0ca36acce8d697dedb
Ryan Fischer <ryan.fischer@20spokes.com>
2011-08-31 18:44:47 -0500

    initiate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geekbusters::Application.config.secret_token = '85a732e66a85c6d7ef9e8361e2e6c2da1714c726bfce5eab42b7f660e4bc2eae381f85acc5672af619e70c3376ccd00c7510b587522ead33195c0f1674a1c49f'
